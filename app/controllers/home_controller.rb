1128e69a5c830ebfbf5592e2e19321b0a309364a
goalista10 <agriam_angelo@yahoo.com.ph>
2021-05-14 15:51:55 +0800

    add header and split for buyer n seller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@
 
 class HomeController < ApplicationController
   def homepage; end
+
+  def signup; end
+
+  def login; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@
 Rails.application.routes.draw do
   devise_for :sellers
   devise_for :buyers
+
+  get 'home/signup'
+  get 'home/login'
   root 'home#homepage'
 end
