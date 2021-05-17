a2c385672435532a4d9635b63a11874715b09d4f
Angelo <goalista@gmail.com>
2021-05-17 19:36:02 +0800

    mailers

---
+++ config/initializers/app_env_vars.rb
@@ -0,0 +1,2 @@
+ENV['MAILER_EMAIL'] = 'for.testing.ange@gmail.com'
+ENV['MAILER_PASSWORD'] = 'testP@$$w0rd10!'
--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2021_05_14_065418) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_buyers_on_email", unique: true
@@ -33,6 +37,10 @@ ActiveRecord::Schema.define(version: 2021_05_14_065418) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_sellers_on_email", unique: true
