8436524ca050e7baec44093dac433aab32ccbc7d
Angelo <goalista@gmail.com>
2021-05-19 20:24:05 +0800

    product crud

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -11,9 +11,36 @@ class ProductController < ApplicationController
                 flash.notice = "Category name is redundant"
             end
         end
-        redirect_to "/"
+        redirect_to "/sellers/products"
     end
 
+    def destroy
+        @to_be_deleted = current_seller.products.find(params[:id])
+        @to_be_deleted.destroy
+        flash.notice = "Category deleted"
+        redirect_to "/sellers/products"
+    end
+
+    def edit
+        @to_be_updated = current_seller.products.find(params[:id])
+        render "product/edit"
+    end
+
+    def update
+        @update_product = current_seller.products.find(params[:id])
+        if @update_product.update(product_params)
+            flash.notice = "Product updated"
+            redirect_to "/sellers/products" 
+        else
+            @error = @update_product.errors.full_messages.first
+            if @error.include? "blank"
+                flash.notice = "Category name can't be blank"
+            else
+                flash.notice = "Category name is redundant"
+            end
+            redirect_to edit_category_path(params[:id])
+        end
+    end
     private
     # Only allow a list of trusted parameters through.
     def product_params
