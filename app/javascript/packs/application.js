932e91c09101049d8517480160f1bf4c0b1059fd
Angelo <goalista@gmail.com>
2021-05-19 22:18:53 +0800

    working js and dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,25 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-require("jquery")
+// export jquery to global
+var jQuery = require('jquery')
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
 require("packs/bootstrap.min")
 require("packs/jquery-3.3.1.min")
 require("packs/jquery-ui.min")
 require("packs/jquery.nice-select.min")
 require("packs/jquery.slicknav")
 require("packs/mixitup.min")
+require("packs/owl.carousel.min")
 require("packs/main")
 require("packs/test")
 
--- app/javascript/packs/application2.js
+++ app/javascript/packs/application2.js
@@ -1,4 +1,11 @@
-require("jquery")
+// export jquery to global
+var jQuery = require('jquery')
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+
 require("packs/bootstrap.min")
 require("packs/jquery-3.3.1.min")
 require("packs/jquery-ui.min")
