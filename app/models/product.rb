3890e6bed6a71775e13a69a1e681383f3735276a
Angelo <goalista@gmail.com>
2021-05-21 17:00:40 +0800

    add validations for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
+    validates :name, presence: true, uniqueness: { scope: :seller_id ,:case_sensitive => false }
+    validates :cost, presence: true
+    validates :pic_url, presence: true
+    validates :description, presence: true
     belongs_to :seller
 end
