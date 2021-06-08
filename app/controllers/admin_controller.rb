a2367d221b13e95fc09bf005e112bf315859d556
Angelo <goalista@gmail.com>
2021-06-08 20:38:41 +0800

    add visible to admin table

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
 
 class AdminController < ApplicationController
-  def dashboard
-    if admin_signed_in?
-      @buyer_count = Buyer.all.count
-      @seller_count = Seller.all.count
-      @product_count = Product.all.count
-    else
-      redirect_to '/admins/sign_in'
-    end
-  end
 
   def buyers
     if admin_signed_in?
